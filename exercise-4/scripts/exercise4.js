65f356352a0f68b1a2f342b873f563cc21f29454
Sivert Sæter <sivertseter@yahoo.com>
2023-11-30 15:54:45 +0100

    done with exercise 3.2

---
+++ exercise-4/scripts/exercise4.js
@@ -0,0 +1,22 @@
+function findHighestNum() {
+    
+    let highestNum = Number.MIN_SAFE_INTEGER;
+
+    while(true) {
+
+        let input = prompt("Enter something here (enter 0 to exit):");
+        
+        let num = parseFloat(input);
+
+        if(num === 0) {
+            break;
+        }
+
+        if(!isNaN(num) && num > highestNum) {
+            highestNum = num;
+        }
+    }
+
+    console.log("The highest number entered is: " + highestNum);
+}
+findHighestNum();
